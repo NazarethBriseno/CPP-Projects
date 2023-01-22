1b4864ecb380c23e6506f3e152c84a8170def896
Nazareth-1 <nazarethbriseno@gmail.com>
2023-01-21 19:51:11 -0600

    Practicing character output and input

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 int main(){
-    
-    std::cout << "Hello World!";
-
+    int yearBorn = 0;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "In what year were you born?" << std::endl;
+    std::cin >> yearBorn;
+    std::cout << yearBorn;
+    return 0;
 }
